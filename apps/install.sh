f55ed7676daa4b977e2c8792ec57b0c918fb0afa
Satoshi Oikawa <oikwsat@gmail.com>
2017-01-21 20:54:08 +0900

    macOS appsのインストールを自動化

---
+++ apps/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+##
+## initialize
+##
+
+# install homebrew
+which brew > /dev/null && echo "homebrew is installed "|| ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install ansible
+which ansible > /dev/null && echo "ansible is installed"  || brew install ansible
+
+# setup by ansible
+ansible-playbook --ask-sudo-pass -i hosts playbook.yml
